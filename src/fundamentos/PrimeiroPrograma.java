652e3f64ed987ad65408e8bf91f8ceb306ee350b
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-04-03 22:34:04 -0300

    Primeiro commit

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+/**
+ * Created by FelipeWendt on 03/04/18.
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Primeiro programa");
+        
+    }
+}
