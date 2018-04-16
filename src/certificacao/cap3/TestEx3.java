887a249e4377a6bf94bf084c91dc47395bd485ea
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-04-16 18:28:01 -0300

    Certificação 2

---
+++ src/certificacao/cap3/TestEx3.java
@@ -0,0 +1,12 @@
+package certificacao.cap3;
+
+public class TestEx3 {
+    public static void main(String[] args) {
+        for (int i =0; i < 20; i++){
+            System.out.println(i);
+        }//letra c
+
+        int i = 15;
+        System.out.println(i);
+    }
+}
