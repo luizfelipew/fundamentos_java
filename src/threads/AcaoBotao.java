4ba66f60d211558f6a56273387b64df49abd5d80
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-11-28 11:38:47 -0300

    Criando a primeira thread

---
+++ src/threads/AcaoBotao.java
@@ -0,0 +1,28 @@
+package threads;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AcaoBotao implements ActionListener {
+
+    private JTextField primeiro;
+    private JTextField segundo;
+    private JLabel resultado;
+
+    public AcaoBotao(final JTextField primeiro, final JTextField segundo, final JLabel resultado) {
+        this.primeiro = primeiro;
+        this.segundo = segundo;
+        this.resultado = resultado;
+    }
+
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        Runnable tarefa = new TarefaMultiplicacao(primeiro, segundo, resultado);
+        Thread threadCalculo = new Thread(tarefa, "Thread Calculadora");
+
+        //thread começa a trabalhar
+        threadCalculo.start();
+    }
+
+}
---
+++ src/threads/Principal.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class Principal {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Isso eh uma thread");
+        Thread.sleep(50000);
+    }
+}
