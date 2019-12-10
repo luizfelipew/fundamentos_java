08ce0ead0b0153ffcb3573fe636b3976cb5fc976
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-12-10 11:10:15 -0300

    Criando primeira conexao via socket servidor - cliente

---
+++ src/threads_servidor/servidor/ServidorTerefas.java
@@ -0,0 +1,19 @@
+package threads_servidor.servidor;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServidorTerefas {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("--- Iniciando servidor ---");
+        ServerSocket servidor = new ServerSocket(12345);
+
+        while (true) {
+            Socket socket = servidor.accept();
+            System.out.println("Aceitando novo cliente na porta: " + socket.getPort());
+        }
+    }
+
+}
