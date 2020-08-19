99f748cd3bf17adeecf8e7377c1088e3aa0214f1
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-08-19 17:05:47 -0300

    testando lambda da inline

---
+++ src/funcional_java8/exer10/Exer10.java
@@ -0,0 +1,17 @@
+package funcional_java8.exer10;
+
+import org.junit.Test;
+
+public class Exer10 {
+
+    public void calcular(Matematica m){
+        System.out.println(m.operar(10, 10));
+    }
+
+    @Test
+    public void exer10(){
+        calcular((v1, v2) -> v1 + v2);
+        calcular((v1, v2) -> v1 - v2);
+        calcular((a, b) -> a * b);
+    }
+}
---
+++ src/funcional_java8/exer10/Matematica.java
@@ -0,0 +1,5 @@
+package funcional_java8.exer10;
+
+public interface Matematica {
+    Integer operar(Integer v1, Integer v2);
+}
