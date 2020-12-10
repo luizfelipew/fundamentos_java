2a22ce1fb2d1e3fd22293cceb0e101eed504112c
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-12-10 20:05:55 -0300

    Aula 185: Java 8 Streams pt 01

---
+++ src/devdojo/java8streams/ZZKstreams/classes/Pessoa.java
@@ -0,0 +1,53 @@
+package devdojo.java8streams.ZZKstreams.classes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private double salario;
+
+    public Pessoa(String nome, int idade, double salario) {
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public static List<Pessoa> bancoDePessoas() {
+        return Arrays.asList(
+                new Pessoa("Luiz Felipe", 35, 2000),
+                new Pessoa("Mercy", 28, 1500),
+                new Pessoa("Ana", 19, 1985),
+                new Pessoa("Thor", 23, 1895),
+                new Pessoa("Hulk", 35, 8000),
+                new Pessoa("Flash", 29, 3200),
+                new Pessoa("Batman", 37, 10000000),
+                new Pessoa("Lanterna verde", 24, 5000)
+        );
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
---
+++ src/devdojo/java8streams/ZZKstreams/test/StreamTest.java
@@ -0,0 +1,38 @@
+package devdojo.java8streams.ZZKstreams.test;
+
+import devdojo.java8streams.ZZKstreams.classes.Pessoa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest {
+    public static void main(String[] args) {
+        List<Pessoa> pessoas = Pessoa.bancoDePessoas();
+        Collections.sort(pessoas, (c1,c2) -> c1.getNome().compareTo(c2.getNome()));
+        List<String> nomes = new ArrayList<>();
+
+        for (Pessoa pessoa: pessoas) {
+            if (pessoa.getIdade() > 25) {
+                nomes.add(pessoa.getNome());
+                if (nomes.size() >= 3){
+                    break;
+                }
+            }
+        }
+
+        System.out.println(nomes);
+        List<String> nomes2 = pessoas
+                .stream()
+                .filter(p -> p.getIdade() > 25)
+                .sorted(Comparator.comparing(Pessoa::getNome))
+                .limit(3)
+                .map(Pessoa::getNome)
+                .collect(Collectors.toList());
+
+        System.out.println("=======================");
+        System.out.println(nomes2);
+    }
+}
